4ab7dcfa96cc1a1f3366befd7b74cc3b4415d482
Matheus Wallace <matheuswts2011@hotmail.com>
2021-12-08 17:34:35 -0300

    add navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img src="" id="logo">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/Pedidos">Pedidos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
