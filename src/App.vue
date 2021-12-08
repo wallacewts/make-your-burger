40412fddbaced9bfa03414f86766b17995f0ec64
Matheus Wallace <matheuswts2011@hotmail.com>
2021-12-08 17:58:37 -0300

    add navbar logo and global style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <NavBar />
+  <NavBar :logo="logo_src" :alt="app_name" />
   <router-view/>
   <Footer />
 </template>
@@ -13,8 +13,32 @@ export default {
     NavBar,
     Footer,
   },
+  data() {
+    return {
+      logo_src: '/img/logo.png',
+      app_name: 'Make Your Burger',
+    };
+  },
 };
 </script>
 
 <style lang="scss">
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 3rem;
+    min-height: 15.5rem;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 2.625em;
+    margin-bottom: 1.8rem;
+    color: #222;
+  }
 </style>
